1621626725a1f008e2a619dc54f3e527a16ca167
Jacob Higgins <higginsjake909@gmail.com>
2021-01-06 23:03:28 -0600

    Blinking TI ARM Cortex M4 led with raw register writes

--- gettingStartedTI/main.c
+++ gettingStartedTI/main.c
@@ -1,15 +1,32 @@
 
 int main()
 {
+    // enabling clock, setting pin direction
     *((unsigned int *)0x400FE608U) = 0x20U;
     *((unsigned int *)0x40025400U) = 0x0EU;
     *((unsigned int *)0x4002551CU) = 0x0EU;
+    
+    while(1)
+    {
+        int counter = 0;
 
-	while(1)
-	{
-		*((unsigned int *)0x400253FCU) = 0x02U;
-		*((unsigned int *)0x400253FCU) = 0x00U;
-	}
+        // led on
+        *((unsigned int *)0x400253FCU) = 0x02U;
+        // slight delay
+        while (counter < 1000000)
+        {
+            ++counter;
+        }        
+
+        counter = 0;
+
+        // off
+        *((unsigned int *)0x400253FCU) = 0x00U;
+        while (counter < 1000000)
+        {
+            ++counter;
+        }
+    }
     
     return 0;
 }
