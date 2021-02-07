0f6abc4ab20946afd7ec4af750dc4852701735fb
Jacob Higgins <higginsjake909@gmail.com>
2021-02-07 00:45:48 -0600

    learning about various integer types and how they are handled by compiler and turned into ARM assembly instructions

--- typesStandardLibraries/main.c
+++ typesStandardLibraries/main.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #include "lm4f120h5qr.h"
 #include "delay.h"
 
@@ -5,21 +7,34 @@
 #define LED_BLUE    (1U << 2)
 #define LED_GREEN   (1U << 3)
 
+/* DEFINES */
 int * swap(int * x, int * y);
 
-int * swap(int * x, int * y)
-{
-    // not necesarily a proper fix, but demonstrates using static to 
-    // keep the variable alive at the beginning of memory through the static keyword
-    static int temp[2];
-    *x = *y;
-    *y = temp;
+/* GLOBALS*/
+uint8_t u8a, u8b;
+uint16_t u16c, u16d;
+uint32_t u32e, u32f;
 
-    return temp;
-}
+int8_t s8;
+int16_t s16;
+int32_t s32;
 
 int main()
 {        
+
+    u8a = sizeof(u8a);
+    u16c = sizeof(uint16_t);
+    u32e = sizeof(uint32_t);
+
+    u8a = 0xa2U;
+    u16c = 0xc23U;
+    u32e =0x1234ee;
+
+    u8b = u8a;
+    u16d = u16c;
+    u32f = u32e;
+
+
     /* Simple Pointer / Swap Excercise */
     int x = 1;
     int y = 3;
@@ -59,3 +74,13 @@ int main()
 }
 
 
+int * swap(int * x, int * y)
+{
+    // not necesarily a proper fix, but demonstrates using static to 
+    // keep the variable alive at the beginning of memory through the static keyword
+    static int temp[2];
+    *x = *y;
+    // *y = temp;
+
+    return temp;
+}
